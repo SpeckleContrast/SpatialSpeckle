fb0b5d31159844fff231284fc82c6f4da4012466
SpeckleContrast <120845306+SpeckleContrast@users.noreply.github.com>
2023-10-26 16:46:20 -0600

    Example.m

--- MATLAB Spatial Contrast Functions/Example.m
+++ MATLAB Spatial Contrast Functions/Example.m
@@ -1,3 +1,6 @@
+% The article "Improved spatial speckle contrast model for tissue blood flow imaging: Effects of spatial correlation among neighboring camera pixels" 
+% in the Journal of Biomedical Optics (JBO) should be cited in any work related to the software.
+
 %%%%%%%%%%%%%%%%% 2p+1 <= sqrt(N)  %%%%%%%%%%%%%%
 %%%%%%%%    M= area pixel/ area speckle   %%%%%%%
 %%%%%%%%   M with exponential separation. %%%%%%%
