38a2ca68626535350b95a7358dc6793b49c8a129
SpeckleContrast <120845306+SpeckleContrast@users.noreply.github.com>
2023-12-12 09:49:01 -0600

    MuInv.m

--- MATLAB Spatial Contrast Functions/MuInv.m
+++ MATLAB Spatial Contrast Functions/MuInv.m
@@ -1,5 +1,7 @@
-% The article "Improved spatial speckle contrast model for tissue blood flow imaging: Effects of spatial correlation among neighboring camera pixels" 
-% in the Journal of Biomedical Optics (JBO) should be cited in any work related to the software.
+% Julio Cesar Juarez-Ramirez, Beatriz Coyotl-Ocelotl, Bernard Choi, Ruben Ramos-Garcia,
+% Teresita Spezzia-Mazzocco, Julio C. Ramirez-San-Juan, 
+% "Improved spatial speckle contrast model for tissue blood flow imaging: effects of spatial correlation among neighboring camera pixels," 
+% J. Biomed. Opt. 28(12) 125002 (5 December 2023) https://doi.org/10.1117/1.JBO.28.12.125002
 
 function RETURN=MuInv(M,eta,xi)
 RETURN=(1./((2*pi.*M).^2)).*factorMu(M,eta).*factorMu(M,xi);
